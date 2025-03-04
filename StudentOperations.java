667005d1ff829976b105a37d6c2f47eef947e567
Aneeka25 <154527297+Aneeka25@users.noreply.github.com>
2025-03-04 22:53:17 +0530

    Update StudentOperations.java with add function

--- StudentOperations.java
+++ StudentOperations.java
@@ -2,3 +2,11 @@ import java.util.*;
 
 class StudentOperations {
     ArrayList<Student> students;
+     public StudentOperations() {
+        this.students = new ArrayList<>();
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+        System.out.println("Student Added");
+    }
