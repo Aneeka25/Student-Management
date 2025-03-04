be98cfa7ef5849350194b701601eca9bce401874
Aneeka25 <154527297+Aneeka25@users.noreply.github.com>
2025-03-04 22:57:48 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+//MAIN.JAVA
+//Name : Aneeka Jain
+//prn : 23070126013
+//Batch : AIML A1
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        StudentOperations operations = new StudentOperations();
+        int choice;
