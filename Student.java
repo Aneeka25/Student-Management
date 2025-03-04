9dc2459eb960caaa236145447a9bf845f2b9050e
Aneeka25 <154527297+Aneeka25@users.noreply.github.com>
2025-03-04 22:49:30 +0530

    Creating  Student.java class and initializing constructors

---
+++ Student.java
@@ -0,0 +1,15 @@
+class Student {
+    private String name;
+    private int prn;
+    private String branch;
+    private String batch;
+    private float cgpa;
+
+
+    public Student(String name, int prn, String branch, String batch, float CGPA) {
+        setName(name);
+        setPRN(prn);
+        setBranch(branch);
+        setBatch(batch);
+        setcgpa(CGPA);
+    }
